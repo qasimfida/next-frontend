00b1a582a66812fab23b1af177f1460fe7ac7999
qasimfida <qasimfida0054@gmail.com>
2021-07-11 13:12:01 -0700

    changes

--- src/component/Loader/index.js
+++ src/component/Loader/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 function Loader() {
-  return <div className="loader">Loading...</div>;
+  return (
+    <div className="loader">
+      <Spinner animation="grow" variant="warning" />
+    </div>
+  );
 }
 
 export default Loader;
