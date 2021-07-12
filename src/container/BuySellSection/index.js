d357bfa172b6597c8bdcc4a207e1db642e92bad0
qasimfida <qasimfida0054@gmail.com>
2021-07-11 18:46:06 -0700

    changes

--- src/container/BuySellSection/index.js
+++ src/container/BuySellSection/index.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React from 'react';
 
-import { Container } from "react-bootstrap";
-import styles from "../../styles/container/BuySell.module.css";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import styles from '../../styles/container/BuySell.module.css';
 
 function BuySellSection() {
   return (
     <div className={styles.buy_wrap}>
       <Container class="container">
-        <h1>Buy or sell your house</h1>
-        <p>
-          Donec placerat dolor id neque pretium placerat. Donec in facilisis
-          risus. In sollicitudin magna luctus sem ultrices convallis. Sed quis
-          ex vel tellus ullamcorper malesuada. Aenean facilisis ex dolor, id
-          vehicula nisl consectetur dapibus. Phasellus mollis mauris semper
-          placerat convallis. Morbi varius facilisis dignissim. Donec eu
-          sollicitudin nunc. Aliquam in nisi id arcu gravida vehicula quis nec
-          sapien. Fusce at dolor ex.
-        </p>
-        <div className={styles.start_btn}>
-          {" "}
-          <a href="#">Start Search Now</a> <a href="#">Browse Properties</a>{" "}
-        </div>
+        <Row className="justify-content-center">
+          <Col sm={12} md={8}>
+            <h1>Buy or sell your house</h1>
+            <p>
+              Donec placerat dolor id neque pretium placerat. Donec in facilisis risus. In
+              sollicitudin magna luctus sem ultrices convallis. Sed quis ex vel tellus ullamcorper
+              malesuada. Aenean facilisis ex dolor, id vehicula nisl consectetur dapibus. Phasellus
+              mollis mauris semper placerat convallis. Morbi varius facilisis dignissim. Donec eu
+              sollicitudin nunc. Aliquam in nisi id arcu gravida vehicula quis nec sapien. Fusce at
+              dolor ex.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button className={`${styles.my_button} ${styles.mar_right}`}>Start Search Now</Button>
+            <Button className={styles.my_button}>Browse Properties</Button>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
